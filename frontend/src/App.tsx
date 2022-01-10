b3f868465b5527de986475d08f89b83b10c03966
Henrique Coppi <riquecoppi@gmail.com>
2022-01-10 17:15:57 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 
-
+import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Pagina Principal</h1>
+   <Navbar />
   );
 }
 
